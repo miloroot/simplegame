1ae0d682da785ea02723b66769d5366ebef5b6b3
Camilo Palma <milonice@hotmail.com>
2017-04-01 14:54:59 +0200

    Added jCanvas to project. Drew a circle.

--- js/main.js
+++ js/main.js
@@ -2,4 +2,20 @@ $(function() {
 
     console.log("jQuery loaded.");
 
+    var gameScreen = $('#gamescreen');
+    
+    var circle = gameScreen.drawArc({
+        name: 'blueCircle',
+        layer: true,
+        fillStyle: 'lightblue',
+        strokeStyle: 'darkblue',
+        strokeWidth: 3,
+        x: 200, y: 200,
+        radius: 50
+    });
+
+    circle.on('click', function() {
+        console.log('Has been clicked: circle.');
+    });
+
 });
